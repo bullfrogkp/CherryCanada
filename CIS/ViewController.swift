5330c4a5991e8f48c5bf2ff02b1c923dc2f55407
pank <pqhitpan@gmail.com>
2019-06-24 13:39:51 -0400

    dddd

--- CIS/ViewController.swift
+++ CIS/ViewController.swift
@@ -8,13 +8,172 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MyTableViewController: UITableViewController {
+    
+    var items = ["Item 1", "Item 2", "Item 3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.title = "My TableView"
+        
+        tableView.register(MyCell.self, forCellReuseIdentifier: "cellId")
+        tableView.register(Header.self, forHeaderFooterViewReuseIdentifier: "headerId")
+        
+        tableView.sectionHeaderHeight = 100
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Insert", style: .plain, target: self, action: Selector(("insert")))
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Batch Insert", style: .plain, target: self, action: Selector(("insertBatch")))
+        
+        self.tableView.rowHeight = 200
     }
+    
+    @objc func insertBatch() {
+        var indexPaths = [NSIndexPath]()
+        for i in items.count...items.count + 5 {
+            items.append("Item \(i + 1)")
+            indexPaths.append(NSIndexPath(row: i, section: 0))
+        }
+        
+        var bottomHalfIndexPaths = [NSIndexPath]()
+        for _ in 0...indexPaths.count / 2 - 1 {
+            bottomHalfIndexPaths.append(indexPaths.removeLast())
+        }
+        
+        tableView.beginUpdates()
+        
+        tableView.insertRows(at: indexPaths as [IndexPath], with: .right)
+        tableView.insertRows(at: bottomHalfIndexPaths as [IndexPath], with: .left)
+        
+        tableView.endUpdates()
+    }
+    
+    @objc func insert() {
+        items.append("Item \(items.count + 1)")
+        
+        let insertionIndexPath = NSIndexPath(row: items.count - 1, section: 0)
+        
+        tableView.insertRows(at: [insertionIndexPath as IndexPath], with: .automatic)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath as IndexPath) as! MyCell
+        myCell.nameLabel.text = items[indexPath.row]
+        myCell.myTableViewController = self
+        return myCell
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            viewForHeaderInSection section: Int) -> UIView? {
+        return tableView.dequeueReusableHeaderFooterView(withIdentifier: "headerId")
+    }
+    
+    func deleteCell(cell: UITableViewCell) {
+        if let deletionIndexPath = tableView.indexPath(for: cell) {
+            items.remove(at: deletionIndexPath.row)
+            tableView.deleteRows(at: [deletionIndexPath], with: .automatic)
+        }
+    }
+    
+}
 
+class Header: UITableViewHeaderFooterView {
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let itemImageView: UIImageView = {
+        let imageName = "test.jpg"
+        let image = UIImage(named: imageName)
+        let imageView = UIImageView(image: image!)
+        let screenSize: CGRect = UIScreen.main.bounds
+        imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        return imageView
+    }()
+    
+    func setupViews() {
+        addSubview(itemImageView)
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": itemImageView]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": itemImageView]))
+        
+    }
+    
+}
 
+class MyCell: UITableViewCell {
+    
+    var myTableViewController: MyTableViewController?
+    var dataSource: ItemTableViewController?
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sample Item"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    let actionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Delete", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let customerNameTextField: UITextField = {
+        let name = UITextField()
+        name.placeholder = "Name"
+        name.translatesAutoresizingMaskIntoConstraints = false
+        name.font = UIFont.boldSystemFont(ofSize: 14)
+        return name
+    }()
+    
+    
+    func setupViews() {
+        addSubview(nameLabel)
+        addSubview(actionButton)
+        addSubview(customerNameTextField)
+        
+        let myTableView = UITableView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        dataSource = ItemTableViewController()
+        myTableView.dataSource = dataSource
+        myTableView.delegate = dataSource
+        addSubview(myTableView)
+        
+        actionButton.addTarget(self, action: Selector(("handleAction")), for: .touchUpInside)
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-8-[v1(80)]-8-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": nameLabel, "v1": actionButton]))
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": actionButton]))
+        
+    }
+    
+    @objc func handleAction() {
+        myTableViewController?.deleteCell(cell: self)
+    }
+    
 }
 
