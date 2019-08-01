fc1486363398159b87d7503ee215f8c238de7e2e
pank <pqhitpan@gmail.com>
2019-08-01 14:27:25 -0400

    bbb

---
+++ CIS/View/ItemListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemListTableViewCell.swift
+//  CIS
+//
+//  Created by Kevin Pan on 2019-08-01.
+//  Copyright © 2019 Kevin Pan. All rights reserved.
+//
+
+import UIKit
+
+class ItemListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
