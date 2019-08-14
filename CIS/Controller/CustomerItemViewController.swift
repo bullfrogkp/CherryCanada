277565df33df92dee07fe33ad4dbc3123793b0fb
pank <pqhitpan@gmail.com>
2019-08-14 11:30:48 -0400

    ddd

---
+++ CIS/Controller/CustomerItemViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomerItemViewController.swift
+//  CIS
+//
+//  Created by Kevin Pan on 2019-08-14.
+//  Copyright © 2019 Kevin Pan. All rights reserved.
+//
+
+import UIKit
+
+class CustomerItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
