5a365d207abc76f21202f88fdd712982af75171e
pank <pqhitpan@gmail.com>
2019-08-20 13:47:41 -0400

    ddd

---
+++ CIS/Controller/ImageItemViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageItemViewController.swift
+//  CIS
+//
+//  Created by Kevin Pan on 2019-08-20.
+//  Copyright © 2019 Kevin Pan. All rights reserved.
+//
+
+import UIKit
+
+class ImageItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
