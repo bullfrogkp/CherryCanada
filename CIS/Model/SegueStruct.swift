5e564861a0dd7819d41d77807c422ca860255e9f
pank <pqhitpan@gmail.com>
2019-08-21 10:29:27 -0400

    dd

---
+++ CIS/Model/SegueStruct.swift
@@ -0,0 +1,19 @@
+//
+//  SegueStruct.swift
+//  CIS
+//
+//  Created by Kevin Pan on 2019-08-21.
+//  Copyright © 2019 Kevin Pan. All rights reserved.
+//
+
+import Foundation
+
+struct CustomerItemData {
+    var customerName: String
+    var items: [Item]
+}
+
+struct ImageItemData {
+    var imageName: String
+    var customers: [Customer]
+}
