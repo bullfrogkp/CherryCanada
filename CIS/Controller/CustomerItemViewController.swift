e92f982cdc515107c32138988ab29f5e4d01d08c
pank <pqhitpan@gmail.com>
2019-08-21 15:37:57 -0400

    eee

--- CIS/Controller/CustomerItemViewController.swift
+++ CIS/Controller/CustomerItemViewController.swift
@@ -23,10 +23,14 @@ class CustomerItemViewController: UIViewController, UITableViewDelegate, UITable
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        customerItemTableView.register(Header.self, forHeaderFooterViewReuseIdentifier: "headerId")
+        
         customerNameLabel.text = pageData.customerName
         
         customerItemTableView.delegate = self
         customerItemTableView.dataSource = self
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "编辑", style: .plain, target: self, action: Selector(("editData")))
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -53,5 +57,114 @@ class CustomerItemViewController: UIViewController, UITableViewDelegate, UITable
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return pageData.images?[section].name ?? ""
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   viewForHeaderInSection section: Int) -> UIView? {
+        let myHeader = tableView.dequeueReusableHeaderFooterView(withIdentifier: "headerId") as! ItemHeader
+        myHeader.customerTableViewController = self
+        myHeader.customerName = shipping.imageName
+        myHeader.setupViews()
+        return myHeader
+    }
+    
+    @objc func editData() {
+        print("Shipping deleted")
+    }
+    
+    @objc func addItem() {
+        shipping.customers.append(Customer())
+        
+        let insertionIndexPath = NSIndexPath(row: shipping.customers.count - 1, section: 0)
+        
+        tableView.insertRows(at: [insertionIndexPath as IndexPath], with: .automatic)
+    }
+    
+    @objc func deleteImage() {
+        print("Shipping deleted")
+    }
+}
+
+
+class ItemHeader: UITableViewHeaderFooterView {
+    var customerTableViewController: CustomerItemViewController!
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let itemImageView: UIImageView = {
+        let imageName = "test.jpg"
+        let image = UIImage(named: imageName)
+        let imageView = UIImageView(image: image!)
+        let screenSize: CGRect = UIScreen.main.bounds
+        imageView.frame = CGRect(x: 10, y: 10, width: screenSize.width - 20, height: 300)
+        
+        imageView.layer.borderColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0).cgColor
+        imageView.layer.cornerRadius = 5.0
+        imageView.layer.borderWidth = 2
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    let addItemButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("添加物品", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = UIColor(red: 0, green: 0.5, blue: 0.8, alpha: 1.0)
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.black.cgColor
+        button.contentEdgeInsets = UIEdgeInsets(top: 15,left: 15,bottom: 15,right: 15)
+        button.setTitleColor(.white, for: .normal)
+        button.sizeToFit()
+        return button
+    }()
+    
+    let deleteImageButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("删除图片", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = UIColor.red
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.black.cgColor
+        button.contentEdgeInsets = UIEdgeInsets(top: 15,left: 15,bottom: 15,right: 15)
+        button.setTitleColor(.white, for: .normal)
+        button.sizeToFit()
+        return button
+    }()
+    
+    func setupViews() {
+        itemImageView.image = UIImage(named: "test2.jpg")
+        addSubview(itemImageView)
+        addSubview(addItemButton)
+        addSubview(deleteImageButton)
+        
+        addItemButton.addTarget(self, action: Selector(("addCustomer")), for: .touchUpInside)
+        deleteImageButton.addTarget(self, action: Selector(("deleteShipping")), for: .touchUpInside)
+        
+        let views: [String: Any] = [
+            "itemImageView": itemImageView,
+            "addItemButton": addItemButton,
+            "deleteImageButton": deleteImageButton
+        ]
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[itemImageView]-|", metrics: nil, views: views))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[deleteImageButton(160)]-20-[addItemButton]-|", options: .alignAllCenterY, metrics: nil, views: views))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[itemImageView]-20-[deleteImageButton]", metrics: nil, views: views))
+    }
+    
+    @objc func addItem() {
+        customerTableViewController.addItem()
+    }
+    
+    @objc func deleteShipping() {
+        customerTableViewController.deleteImage()
     }
 }
