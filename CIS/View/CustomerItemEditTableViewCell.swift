bc34c752e4472b7952989567608a2f98a2ab4e50
pank <pqhitpan@gmail.com>
2019-08-22 10:56:15 -0400

    ffff

---
+++ CIS/View/CustomerItemEditTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomerItemEditTableViewCell.swift
+//  CIS
+//
+//  Created by Kevin Pan on 2019-08-22.
+//  Copyright © 2019 Kevin Pan. All rights reserved.
+//
+
+import UIKit
+
+class CustomerItemEditTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CIS/View/ImageItemEditTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ImageItemEditTableViewCell.swift
+//  CIS
+//
+//  Created by Kevin Pan on 2019-08-22.
+//  Copyright © 2019 Kevin Pan. All rights reserved.
+//
+
+import UIKit
+
+class ImageItemEditTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
