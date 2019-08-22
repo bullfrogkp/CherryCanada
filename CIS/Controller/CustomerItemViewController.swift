e9e297b3440e899b2017b1e5dcf056f29be55552
pank <pqhitpan@gmail.com>
2019-08-22 15:53:55 -0400

    rrr

--- CIS/Controller/CustomerItemViewController.swift
+++ CIS/Controller/CustomerItemViewController.swift
@@ -58,102 +58,29 @@ class CustomerItemViewController: UIViewController, UITableViewDelegate, UITable
     func tableView(_ tableView: UITableView,
                    viewForHeaderInSection section: Int) -> UIView? {
         
-        let headerView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: tableView.frame.width, height: 50))
+        let headerView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: tableView.frame.width, height: 200))
         
-        let label = UILabel()
-        label.frame = CGRect.init(x: 5, y: 5, width: headerView.frame.width-10, height: headerView.frame.height-10)
-        label.text = "Notification Times"
+        let itemImageView: UIImageView = {
+            let imageName = pageData.images![section].name
+            let image = UIImage(named: imageName)
+            let imageView = UIImageView(image: image!)
+            let screenSize: CGRect = UIScreen.main.bounds
+            imageView.frame = CGRect(x: 10, y: 10, width: screenSize.width - 20, height: 200)
+            
+            imageView.layer.borderColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0).cgColor
+            imageView.layer.cornerRadius = 5.0
+            imageView.layer.borderWidth = 2
+            imageView.contentMode = .scaleAspectFit
+            
+            return imageView
+        }()
         
-        headerView.addSubview(label)
+        headerView.addSubview(itemImageView)
         
         return headerView
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 50
-    }
-    
-    @objc func editData() {
-        print("Shipping deleted")
-    }
-    
-    @objc func addItem(_ section: Int) {
-        let insertionIndexPath = NSIndexPath(row: pageData.images![section].items.count - 1, section: section)
-        
-        tableView.insertRows(at: [insertionIndexPath as IndexPath], with: .automatic)
-    }
-    
-    @objc func deleteImage() {
-        print("Shipping deleted")
-    }
-}
-
-
-class ItemHeader: UITableViewHeaderFooterView {
-    var customerTableViewController: CustomerItemViewController!
-    
-    override init(reuseIdentifier: String?) {
-        super.init(reuseIdentifier: reuseIdentifier)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    let itemImageView: UIImageView = {
-        let imageName = "test.jpg"
-        let image = UIImage(named: imageName)
-        let imageView = UIImageView(image: image!)
-        let screenSize: CGRect = UIScreen.main.bounds
-        imageView.frame = CGRect(x: 10, y: 10, width: screenSize.width - 20, height: 300)
-        
-        imageView.layer.borderColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0).cgColor
-        imageView.layer.cornerRadius = 5.0
-        imageView.layer.borderWidth = 2
-        imageView.contentMode = .scaleAspectFit
-        
-        return imageView
-    }()
-    
-    let deleteImageButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("删除图片", for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = UIColor.red
-        button.layer.cornerRadius = 5
-        button.layer.borderWidth = 1
-        button.layer.borderColor = UIColor.black.cgColor
-        button.contentEdgeInsets = UIEdgeInsets(top: 15,left: 15,bottom: 15,right: 15)
-        button.setTitleColor(.white, for: .normal)
-        button.sizeToFit()
-        return button
-    }()
-    
-    func setupViews() {
-        itemImageView.image = UIImage(named: "test2.jpg")
-        addSubview(itemImageView)
-        addSubview(addItemButton)
-        addSubview(deleteImageButton)
-        
-        addItemButton.addTarget(self, action: Selector(("addCustomer")), for: .touchUpInside)
-        deleteImageButton.addTarget(self, action: Selector(("deleteShipping")), for: .touchUpInside)
-        
-        let views: [String: Any] = [
-            "itemImageView": itemImageView,
-            "addItemButton": addItemButton,
-            "deleteImageButton": deleteImageButton
-        ]
-        
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[itemImageView]-|", metrics: nil, views: views))
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[deleteImageButton(160)]-20-[addItemButton]-|", options: .alignAllCenterY, metrics: nil, views: views))
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[itemImageView]-20-[deleteImageButton]", metrics: nil, views: views))
-    }
-    
-    @objc func addItem() {
-        customerTableViewController.addItem()
-    }
-    
-    @objc func deleteShipping() {
-        customerTableViewController.deleteImage()
+        return 200
     }
 }
