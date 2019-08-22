f27a5f057650db08ce9aa8e3ac14c2d1fda38cfd
pank <pqhitpan@gmail.com>
2019-08-22 13:49:09 -0400

    ddd

--- CIS/Controller/CustomerItemEditViewController.swift
+++ CIS/Controller/CustomerItemEditViewController.swift
@@ -10,21 +10,151 @@ import UIKit
 
 class CustomerItemEditViewController: UIViewController {
 
+    var pageData: CustomerItemData!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        customerItemTableView.register(Header.self, forHeaderFooterViewReuseIdentifier: "headerId")
+        
+        customerNameLabel.text = pageData.customerName
+        
+        customerItemTableView.delegate = self
+        customerItemTableView.dataSource = self
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "编辑", style: .plain, target: self, action: Selector(("editData")))
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return pageData.images?.count ?? 0
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pageData.images?[section].items.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customerItemId", for: indexPath) as! CustomerItemTableViewCell
+        
+        let item = pageData.images![indexPath.section].items[indexPath.row]
+        
+        cell.nameLabel.text = item.name
+        cell.quantityLabel.text = "/(item.quantity)"
+        cell.priceSoldLabel.text = "/(item.priceSold)"
+        cell.priceBoughtLabel.text = "/(item.priceBought)"
+        cell.descriptionTextView.text = "/(item.description)"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return pageData.images?[section].name ?? ""
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   viewForHeaderInSection section: Int) -> UIView? {
+        let myHeader = tableView.dequeueReusableHeaderFooterView(withIdentifier: "headerId") as! ItemHeader
+        myHeader.customerTableViewController = self
+        myHeader.customerName = shipping.imageName
+        myHeader.setupViews()
+        return myHeader
+    }
+    
+    @objc func editData() {
+        print("Shipping deleted")
+    }
+    
+    @objc func addItem(_ section: Int) {
+        let insertionIndexPath = NSIndexPath(row: pageData.images![section].items.count - 1, section: section)
+        
+        tableView.insertRows(at: [insertionIndexPath as IndexPath], with: .automatic)
+    }
+    
+    @objc func deleteImage() {
+        print("Shipping deleted")
+    }
+}
 
+
+class ItemHeader: UITableViewHeaderFooterView {
+    var customerTableViewController: CustomerItemViewController!
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let itemImageView: UIImageView = {
+        let imageName = "test.jpg"
+        let image = UIImage(named: imageName)
+        let imageView = UIImageView(image: image!)
+        let screenSize: CGRect = UIScreen.main.bounds
+        imageView.frame = CGRect(x: 10, y: 10, width: screenSize.width - 20, height: 300)
+        
+        imageView.layer.borderColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0).cgColor
+        imageView.layer.cornerRadius = 5.0
+        imageView.layer.borderWidth = 2
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    let addItemButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("添加物品", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = UIColor(red: 0, green: 0.5, blue: 0.8, alpha: 1.0)
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.black.cgColor
+        button.contentEdgeInsets = UIEdgeInsets(top: 15,left: 15,bottom: 15,right: 15)
+        button.setTitleColor(.white, for: .normal)
+        button.sizeToFit()
+        return button
+    }()
+    
+    let deleteImageButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("删除图片", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = UIColor.red
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.black.cgColor
+        button.contentEdgeInsets = UIEdgeInsets(top: 15,left: 15,bottom: 15,right: 15)
+        button.setTitleColor(.white, for: .normal)
+        button.sizeToFit()
+        return button
+    }()
+    
+    func setupViews() {
+        itemImageView.image = UIImage(named: "test2.jpg")
+        addSubview(itemImageView)
+        addSubview(addItemButton)
+        addSubview(deleteImageButton)
+        
+        addItemButton.addTarget(self, action: Selector(("addCustomer")), for: .touchUpInside)
+        deleteImageButton.addTarget(self, action: Selector(("deleteShipping")), for: .touchUpInside)
+        
+        let views: [String: Any] = [
+            "itemImageView": itemImageView,
+            "addItemButton": addItemButton,
+            "deleteImageButton": deleteImageButton
+        ]
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[itemImageView]-|", metrics: nil, views: views))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[deleteImageButton(160)]-20-[addItemButton]-|", options: .alignAllCenterY, metrics: nil, views: views))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[itemImageView]-20-[deleteImageButton]", metrics: nil, views: views))
+    }
+    
+    @objc func addItem() {
+        customerTableViewController.addItem()
+    }
+    
+    @objc func deleteShipping() {
+        customerTableViewController.deleteImage()
+    }
 }
--- CIS/Controller/ImageItemEditViewController.swift
+++ CIS/Controller/ImageItemEditViewController.swift
@@ -10,21 +10,39 @@ import UIKit
 
 class ImageItemEditViewController: UIViewController {
 
+    var pageData: ImageItemData!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        itemImageView.image = UIImage(contentsOfFile: pageData.imageName!)
+        customerItemTableView.delegate = self
+        customerItemTableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return pageData.customers?.count ?? 0
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pageData.customers?[section].items.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customerItemId", for: indexPath) as! CustomerItemTableViewCell
+        
+        let item = pageData.customers![indexPath.section].items[indexPath.row]
+        
+        cell.nameLabel.text = item.name
+        cell.quantityLabel.text = "/(item.quantity)"
+        cell.priceSoldLabel.text = "/(item.priceSold)"
+        cell.priceBoughtLabel.text = "/(item.priceBought)"
+        cell.descriptionTextView.text = "/(item.description)"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return pageData.customers?[section].name ?? ""
+    }
 }
