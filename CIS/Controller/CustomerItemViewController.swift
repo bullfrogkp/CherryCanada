0f244243ea46394cc79b678df263a817bd580a5b
pank <pqhitpan@gmail.com>
2019-08-22 16:08:50 -0400

    ddd

--- CIS/Controller/CustomerItemViewController.swift
+++ CIS/Controller/CustomerItemViewController.swift
@@ -54,21 +54,29 @@ class CustomerItemViewController: UIViewController, UITableViewDelegate, UITable
     func tableView(_ tableView: UITableView,
                    viewForHeaderInSection section: Int) -> UIView? {
         
-        let headerView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: tableView.frame.width, height: 50))
+        let headerView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: tableView.frame.width, height: 200))
         
-        let customerTextField: UITextField = {
-            let cTextField =  UITextField(frame: CGRect(x: 20, y: 100, width: 300, height: 40))
-            cTextField.placeholder = "客户"
+        let itemImageView: UIImageView = {
+            let imageName = pageData.images![section].name
+            let image = UIImage(named: imageName)
+            let imageView = UIImageView(image: image!)
+            let screenSize: CGRect = UIScreen.main.bounds
+            imageView.frame = CGRect(x: 10, y: 10, width: screenSize.width - 20, height: 200)
             
-            return cTextField
+            imageView.layer.borderColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0).cgColor
+            imageView.layer.cornerRadius = 5.0
+            imageView.layer.borderWidth = 2
+            imageView.contentMode = .scaleAspectFit
+            
+            return imageView
         }()
         
-        headerView.addSubview(customerTextField)
+        headerView.addSubview(itemImageView)
         
         return headerView
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 50
+        return 200
     }
 }
