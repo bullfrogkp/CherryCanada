d01d7896fe5667d4edf56f269349122e8ab12c34
pank <pqhitpan@gmail.com>
2019-09-10 17:03:32 -0400

    ddd

--- CIS/Controller/ShippingDetailViewController.swift
+++ CIS/Controller/ShippingDetailViewController.swift
@@ -2,94 +2,66 @@
 //  ShippingDetailViewController.swift
 //  CIS
 //
-//  Created by Kevin Pan on 2019-09-08.
+//  Created by Kevin Pan on 2019-09-10.
 //  Copyright © 2019 Kevin Pan. All rights reserved.
 //
 
 import UIKit
 
-class ShippingDetailViewController: UIViewController {
-
-    @IBOutlet weak var shippingDateTextField: UITextField!
-    @IBOutlet weak var shippingStatusTextField: UITextField!
-    @IBOutlet weak var shippingCityTextField: UITextField!
-    @IBOutlet weak var shippingFeeNationalTextField: UITextField!
-    @IBOutlet weak var shippingFeeInternationalTextField: UITextField!
-    @IBOutlet weak var shippingDepositTextField: UITextField!
-    @IBOutlet weak var shippingCommentTextField: UITextField!
+class ShippingDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBAction func unwind(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-    }
-    @IBAction func saveData(_ sender: Any) {
-        
-        if shippingDateTextField.text == "" {
-            let alertController = UIAlertController(title: "必填项目", message: "请填写日期", preferredStyle: .alert)
-            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-            alertController.addAction(alertAction)
-            present(alertController, animated: true, completion: nil)
-            
-            return
-        }
-        
-        if shipping == nil {
-            shipping = Shipping()
-            shippings.append(shipping!)
-        }
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        shipping!.shippingDate = dateFormatter.date(from: shippingDateTextField.text!)!
-        
-        shipping!.shippingStatus = shippingStatusTextField.text!
-        shipping!.city = shippingCityTextField.text!
-        shipping!.comment = shippingCommentTextField.text!
-        
-        let formatter = NumberFormatter()
-        formatter.generatesDecimalNumbers = true
-        formatter.numberStyle = NumberFormatter.Style.decimal
-        
-        if let formattedNumber = formatter.number(from: shippingFeeNationalTextField.text!) as? NSDecimalNumber  {
-            shipping!.priceNational = formattedNumber as Decimal
-        }
-        
-        if let formattedNumber = formatter.number(from: shippingFeeInternationalTextField.text!) as? NSDecimalNumber  {
-            shipping!.priceInternational = formattedNumber as Decimal
-        }
-        
-        if let formattedNumber = formatter.number(from: shippingDepositTextField.text!) as? NSDecimalNumber  {
-            shipping!.deposit = formattedNumber as Decimal
-        }
-        
-        dismiss(animated: true, completion: nil)
-    }
+    @IBOutlet weak var shippingDateLabel: UILabel!
+    @IBOutlet weak var shippingStatusLabel: UILabel!
+    @IBOutlet weak var shippingCityLabel: UILabel!
+    @IBOutlet weak var shippingPriceNationalLabel: UILabel!
+    @IBOutlet weak var shippingPriceInternationalLabel: UILabel!
+    @IBOutlet weak var shippingDepositLabel: UILabel!
+    @IBOutlet weak var shippingCommentLabel: UILabel!
+    @IBOutlet weak var customerItemTableView: UITableView!
     
-    var shipping: Shipping?
+    var shipping: Shipping!
     var shippings: [Shipping]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if shipping != nil {
+        
+        shippingDateLabel.text = "\(shipping.shippingDate)"
+        shippingStatusLabel.text = shipping.shippingStatus
+        shippingCityLabel.text = shipping.city
+        shippingPriceNationalLabel.text = "\(shipping.priceNational)"
+        shippingPriceInternationalLabel.text = "\(shipping.priceInternational)"
+        shippingDepositLabel.text = "\(shipping.deposit)"
+        shippingCommentLabel.text = "\(shipping.comment)"
+        
+        customerItemTableView.dataSource = self
+        customerItemTableView.delegate = self
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "addCustomer" {
+            var pageData = CustomerItemData()
             
-            let dateFormatterPrint = DateFormatter()
-            dateFormatterPrint.dateFormat = "yyyy-MM-dd"
+            pageData.customerName = ""
+            pageData.images = []
             
-            shippingDateTextField.text = dateFormatterPrint.string(from: shipping!.shippingDate)
-            shippingStatusTextField.text = "\(shipping!.shippingStatus)"
-            shippingCityTextField.text = "\(shipping!.city)"
-            shippingFeeNationalTextField.text = "\(shipping!.priceNational)"
-            shippingFeeInternationalTextField.text = "\(shipping!.priceInternational)"
-            shippingDepositTextField.text = "\(shipping!.deposit)"
-            shippingCommentTextField.text = "\(shipping!.comment)"
-        }   else {
-            shippingDateTextField.text = ""
-            shippingStatusTextField.text = ""
-            shippingCityTextField.text = ""
-            shippingFeeNationalTextField.text = ""
-            shippingFeeInternationalTextField.text = ""
-            shippingDepositTextField.text = ""
-            shippingCommentTextField.text = ""
+            let naviView: UINavigationController = segue.destination as!  UINavigationController
+            let customerView: CustomerItemEditViewController = naviView.viewControllers[0] as! CustomerItemEditViewController
+            
+            customerView.pageData = pageData
+        } else if segue.identifier == "editShippingDetail" {
+            let naviView: UINavigationController = segue.destination as!  UINavigationController
+            let shippingView: ShippingDetailViewController = naviView.viewControllers[0] as! ShippingDetailViewController
+            shippingView.shipping = shipping
+            shippingView.shippings = shippings
         }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
     }
 }
