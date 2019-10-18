dabec62fbf1bf2bd5e6b12f8b1c1f1aff811047f
pank <pqhitpan@gmail.com>
2019-10-18 11:19:11 -0400

    ddd

---
+++ CIS/View/ImageItemSectionHeaderView.swift
@@ -0,0 +1,16 @@
+//
+//  ImageItemSectionHeaderView.swift
+//  CIS
+//
+//  Created by Kevin Pan on 2019-10-18.
+//  Copyright © 2019 Kevin Pan. All rights reserved.
+//
+
+import UIKit
+
+class ImageItemSectionHeaderView: UITableViewHeaderFooterView {
+
+    @IBOutlet weak var deleteCustomerButton: UIButton!
+    @IBOutlet weak var customerTextField: UITextField!
+    @IBOutlet weak var addItemButton: UIButton!
+}
